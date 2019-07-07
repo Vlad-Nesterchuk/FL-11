fc2ecea1975ac576efce3f6f4f66a39fd4004012
Vladislav Nesterchuk <51543440+Vlad-Nesterchuk@users.noreply.github.com>
2019-07-07 23:46:57 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,20 +1,19 @@
-// Your code goes here
 let firstLength = +prompt('Enter length of the first side of the triangle');
 let secondLength = +prompt('Enter length of the second side of the triangle');
 let thirdLength = +prompt('Enter length of the third side of the triangle');
 
 let maxSideLength = Math.max(firstLength, secondLength, thirdLength);
 
-if (maxSideLength < firstLength + secondLength + thirdLength - maxSideLength ){
+if (maxSideLength < firstLength + secondLength + thirdLength - maxSideLength){
     if(firstLength === secondLength && firstLength === thirdLength && secondLength === thirdLength){
         console.log('Equivalent triangle');
     } else {
-        if (firstLength !== secondLength && firstLength !== thirdLength && secondLength !== thirdLength) {
+        if (firstLength !== secondLength && firstLength !== thirdLength && secondLength !== thirdLength){
             console.log('Normal triangle');
         } else {
             console.log('Isosceles triangle');
         }
     } 
-}else {
+} else {
     console.log('Triangle doesn\'t exist');
 }
